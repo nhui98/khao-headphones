e7afe4fd1195f6079349ec495906db8e36ed4935
Nathan <hui.n1998@gmail.com>
2022-04-23 20:30:03 +0100

    add footer banner, hero banner, product component

--- components/product.jsx
+++ components/product.jsx
@@ -1,3 +1,25 @@
-const Product = () => {};
+import Link from 'next/link';
+
+import { urlFor } from '../lib/client';
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div className="">
+      <Link href={`/product/${slug.current}`} passHref>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            alt=""
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default Product;
